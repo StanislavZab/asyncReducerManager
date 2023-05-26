7ed40fc39222f970b74bfb8084d81f4c42ce6fc6
Stanislav Zaborskiy <zaborskiy-stanislav@yandex.ru>
2023-05-26 15:06:59 +0500

    1.0.1

--- index.ts
+++
@@ -1,4 +0,0 @@
-export { DynamicModuleLoader } from './src/components/DynamicModuleLoader';
-export { useReducerManager } from './src/hooks/useReducerManager';
-export { createReducerManager } from './src/reducerManager/reducerManager';
-export type{ ReducerManager, ReduxStoreWithManager } from './src/reducerManager/reducerManager';
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { DynamicModuleLoader } from './components/DynamicModuleLoader';
+export { useReducerManager } from './hooks/useReducerManager';
+export { createReducerManager } from './reducerManager/reducerManager';
+export type{ ReducerManager, ReduxStoreWithManager } from './reducerManager/reducerManager';
